0f0622c63d1f8a784811fae044f1fd38f203aa5f
Humberto Galdino <humberto.galdino@live.com>
2024-12-03 12:31:18 -0300

    update

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 
 export default function Logo() {
-    
-    const spanClass = ''
 
     return (
         <Link href="/" className='text-almostWhite font-bold text-4xl italic'>
