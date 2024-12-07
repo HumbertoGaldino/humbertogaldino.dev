444ea187261efa6064948bdc1e3191ae3e695884
Humberto Galdino <humberto.galdino@live.com>
2024-12-07 17:33:39 -0300

    Adicionando Rotion aos projetos

--- src/components/Card/content.ts
+++ src/components/Card/content.ts
@@ -1,8 +1,24 @@
-import {css3, express, git, html, nextJs, nodeJs, react, sqlite, styledComponents, tailwind, typeScript} from '../HardSkills'
+import {css3, electron, express, git, html, nextJs, nodeJs, react, sqlite, styledComponents, tailwind, typeScript} from '../HardSkills'
 
 export const content = [
-  { 
+  {  
+    id:6,
+    img: 'projeto6.png',
+    title: "Rotion - App Desktop",
+    skills: [electron,react,typeScript, git],
+    github: 'https://github.com/HumbertoGaldino/rotion',
+    description: 'Aplicação fullstack com NextJs e Node.js, exibindo projetos em uma interface responsiva.'
+  },
+  {  
     id:5,
+    img: 'projeto1.webp',
+    title: "Portfólio",
+    skills: [nodeJs ,nextJs, typeScript, tailwind, ],
+    github: 'https://github.com/HumbertoGaldino/humbertogaldino.dev',
+    description: 'Aplicação fullstack com NextJs e Node.js, exibindo projetos em uma interface responsiva.'
+  },
+  {
+    id:4,
     img: 'projeto5.png',
     title: "FoodExplorer - API",
     skills: [nodeJs, express, sqlite, git],
@@ -10,7 +26,7 @@ export const content = [
     description: 'API desenvolvida para uma aplicação de cardápio digital, parte do projeto Food Explorer. Desenvolvido para a formação Explorer da Rocketseat.'
   },
   { 
-    id:4,
+    id:3,
     img: 'projeto4.png',
     title: "FoodExplorer - Front",
     skills: [nodeJs, react, styledComponents, git],
@@ -18,7 +34,7 @@ export const content = [
     description: 'Cardápio digital Food Explorer. Projeto desenvolvido para conclusão da formação Explorer da Rocketseat.'
   },
   { 
-    id:3,
+    id:2,
     img: 'projeto3.png',
     title: "Login Responsivo",
     skills: [html , css3, git ],
@@ -26,20 +42,12 @@ export const content = [
     description: 'Formulário de login responsivo desenvolvido com HTML e CSS, com transições suaves. '
   },
   { 
-    id:2,
+    id:1,
     img: 'projeto2.png',
     title: "Landing Page - Fylo",
     skills: [html , css3, git ],
     github: 'https://github.com/HumbertoGaldino/Projeto-Front-End-Mentor--1',
     description: 'Landing Page responsiva criada com HTML5 e CSS3, baseada no desafio do Frontend Mentor, destacando design moderno e clean.'
-  },
-  { 
-    id:1,
-    img: 'projeto1.webp',
-    title: "Portfólio",
-    skills: [nodeJs ,nextJs, typeScript, tailwind, ],
-    github: 'https://github.com/HumbertoGaldino/humbertogaldino.dev',
-    description: 'Aplicação fullstack com NextJs e Node.js, exibindo projetos em uma interface responsiva.'
   },
 ];
 
